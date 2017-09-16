0a6d6088751c4a79d98ed6dc1571466c5a826205
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2017-09-16 15:12:46 -0500

    Ejercicio 2 del taller de js profesional

---
+++ ejercicios2.js
@@ -0,0 +1,10 @@
+// // 2  Recibir dos números usando prompt y sumarlos, restarlos, dividirlos y multiplicarlos
+
+
+let num1 = parseInt(prompt());
+let num2 = parseInt(prompt());
+
+console.log( num1 +  num2 );
+console.log(num1 - num2);
+console.log(num1/ num2);
+console.log(num1 *  num2);
